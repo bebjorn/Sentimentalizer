01863d012c4ed8b553245624a589c6dcd78e908d
Björn <bjorn.berntsson@gmail.com>
2013-04-17 14:34:09 +0200

    added out of domain

--- Perceptron/Code/Main.m
+++ Perceptron/Code/Main.m
@@ -1,1 +1,8 @@
-crossValidation('dvd_pos', 'dvd_neg', 10)
+%results = zeros(10, 10);
+
+%y = crossValidationCatagory('dvd_pos', 'music_pos', 10);
+
+%results = crossValidation('health_pos', 'health_neg', 10)
+results = outOfDomainValidation('health_pos', 'health_neg', 'books_pos', 'books_neg', 10)
+
+% z = matrix1 ~= 0;
