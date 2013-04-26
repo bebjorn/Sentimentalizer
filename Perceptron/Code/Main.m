d8bf72bd7f5f77b420987f4f7c122a4f612ee003
Björn <bjorn.berntsson@gmail.com>
2013-04-26 17:42:44 +0200

    KNN

--- Perceptron/Code/Main.m
+++ Perceptron/Code/Main.m
@@ -1,6 +1,17 @@
 %results = zeros(10, 10);
 
-[correctness, count] = crossValidationCategory(10);
+%[correctness, count] = crossValidationCategory(10);
+
+%[correctness1] = kNearestNeighbour('books_pos', 'books_neg', 30)
+%[correctness2] = kNearestNeighbour('camera_pos', 'camera_neg', 10)
+%[correctness3] = kNearestNeighbour('dvd_pos', 'dvd_neg', 10)
+%[correctness4] = kNearestNeighbour('health_pos', 'health_neg', 10)
+%[correctness5] = kNearestNeighbour('music_pos', 'music_neg', 10)
+%[correctness6] = kNearestNeighbour('software_pos', 'software_neg', 10)
+%[correctness5] = kNearestNeighbour('books_pos', 'music_neg', 10)
+%[correctness6] = kNearestNeighbour('software_pos', 'software_neg', 10)
+
+[correctness] = KNearestNeigborsCategories('10');
 
 %results1 = crossValidation('books_pos', 'books_neg', 10)
 %results2 = crossValidation('camera_pos', 'camera_neg', 10)
